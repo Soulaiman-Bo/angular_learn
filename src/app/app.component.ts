b0c3786833da6ca3e45eccf12e1757e4ae580a6c
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-03-30 22:55:27 +0000

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +8,7 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'angular_Learn';
-  data: any[] = [];
-
-  myObservable = new Observable((observer) => {
-    setTimeout(()=> {observer.next(1);}, 500)
-    setTimeout(()=> {observer.next(2);}, 1000)
-    setTimeout(()=> {observer.next(3);}, 1500)
-    setTimeout(()=> {observer.next(4);}, 2000)
-    setTimeout(()=> {observer.next(5);}, 2500)
-  });
-
-  GetAsyncData() {
-    this.myObservable.subscribe((val: any) => {
-      this.data.push(val);
-    });
-  }
+ 
 }
