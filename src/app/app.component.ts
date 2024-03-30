1204cea2b5bc3366112d2401851e379ce722e7d0
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-03-30 22:38:33 +0000

    observers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,20 +15,48 @@ export class AppComponent {
   data: any[] = [];
 
   myObservable = new Observable((observer) => {
-    setTimeout(()=> {observer.next(1);}, 1000)
-    setTimeout(()=> {observer.next(2);}, 2000)
-    setTimeout(()=> {observer.next(3);}, 3000)
-    setTimeout(()=> {observer.error(new Error('something went wrong'))}, 3500)
-    setTimeout(()=> {observer.next(4);}, 4000)
-    setTimeout(()=> {observer.next(5);}, 5000)
+    setTimeout(() => {
+      observer.next(1);
+    }, 1000);
+    setTimeout(() => {
+      observer.next(2);
+    }, 2000);
+    setTimeout(() => {
+      observer.next(3);
+    }, 3000);
+    // setTimeout(()=> {observer.error(new Error('something went wrong'))}, 3500)
+    setTimeout(() => {
+      observer.next(4);
+    }, 4000);
+    setTimeout(() => {
+      observer.next(5);
+    }, 5000);
+    setTimeout(() => {
+      observer.complete();
+    }, 6000);
   });
 
   GetAsyncData() {
-    this.myObservable.subscribe((val: any) => {
-      this.data.push(val);
-    },
-    (error)=>{
-        alert(error.message)
+    // this.myObservable.subscribe((val: any) => {
+    //   this.data.push(val);
+    // },
+    // (error)=>{
+    //     alert(error.message)
+    // },
+    // ()=> {
+    //     alert("All the data is Complete")
+    // } );
+
+    this.myObservable.subscribe({
+      next: (val) => {
+        this.data.push(val);
+      },
+      error(error) {
+        alert(error.message);
+      },
+      complete() {
+        alert('All the data is Complete');
+      },
     });
   }
 }
